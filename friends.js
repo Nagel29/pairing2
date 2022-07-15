dfc7275566edba97557281e9a37d59c4a13adfe6
Ryan Nagel <RNagel29@gmail.com>
2022-07-14 18:55:01 -0600

    Add overThirty function

---
+++ friends.js
@@ -0,0 +1,9 @@
+var numbers = [5, 15, 20, 25, 30, 35, 40, 45]
+
+function overThirty(nums) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] > 30) {
+      console.log(nums[i]);
+    }
+  }
+}
